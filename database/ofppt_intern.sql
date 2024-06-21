768b7efd8307a798d2b036b6d67f482e13a09fb8
Hiba ERRAOUI <125480922+er-hiba@users.noreply.github.com>
2024-06-21 03:13:41 +0100

    :)

--- database/ofppt_intern.sql
+++ database/ofppt_intern.sql
@@ -1,1 +1,57 @@
+CREATE DATABASE  IF NOT EXISTS `ofppt` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `ofppt`;
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: localhost    Database: ofppt
+-- ------------------------------------------------------
+-- Server version	8.0.36
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `intern`
+--
+
+DROP TABLE IF EXISTS `intern`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `intern` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `lastName` varchar(45) DEFAULT NULL,
+  `firstName` varchar(45) DEFAULT NULL,
+  `age` int DEFAULT NULL,
+  `login` varchar(100) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `intern`
+--
+
+LOCK TABLES `intern` WRITE;
+/*!40000 ALTER TABLE `intern` DISABLE KEYS */;
+INSERT INTO `intern` VALUES (1,'ERRAOUI','Hiba',20,'erraoui','P@ssw0rd123'),(2,'ESSADAQ','Aabla',20,'essadaq','P@ssw0rd124'),(3,'MIROUANE','Saad',21,'saadoun','P@ssw0rd125'),(5,'JAMAOUI','Mouad',29,'jamaoui','P1234'),(6,'HAMDAOUI','Youssra',25,'hyoussra','1569Pd'),(9,'LEKHLIFI','Zineb',19,'zainab','P15639'),(10,'CHAMRAH','Ziad',19,'chamrah','P@ssword');
+/*!40000 ALTER TABLE `intern` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-06-21  3:10:11
